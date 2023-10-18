5e075c174b0479fdcb65ae3a8bca545a7ba06fc9
saan00002 <saan00002@stud.uni-saarland.de>
2023-10-18 14:24:17 +0200

    Complete bonus for Ruby homework

--- ruby/app/models/article.rb
+++ ruby/app/models/article.rb
@@ -1,3 +1,13 @@
-class Article < ActiveRecord::Base 
+class Article < ActiveRecord::Base
   self.table_name = 'articles'
+
+
+  has_many :comment
+end
+
+class Comment < ActiveRecord::Base
+  self.table_name = 'comment'
+
+  belongs_to :article,
+  foreign_key: "article_id"
 end
--- ruby/app/models/db_init.rb
+++ ruby/app/models/db_init.rb
@@ -8,10 +8,17 @@ DB = ActiveRecord::Base.establish_connection(
   :password => ENV['DB_PASS'],
 )
 
-ActiveRecord::Base.connection.create_table(:articles, primary_key: 'id', force: true) do |t|
+ActiveRecord::Base.connection.create_table(:articles, primary_key: 'id', force: :cascade) do |t|
     t.string :title
     t.string :content
     t.string :created_at
+end
+
+ActiveRecord::Base.connection.create_table(:comment, primary_key: 'id', force: :cascade) do |t|
+  t.belongs_to  :article , foreign_key: true
+  t.text :content
+  t.timestamp :created_at
+  t.string :author_name
 end
 
 require_relative 'article'
@@ -22,3 +29,11 @@ Article.create(:title => 'Title YNN', :content => 'O_O_Y_O_O', :created_at => Ti
 
 puts "Article count in DB: #{Article.count}"
 
+
+Comment.create(:article_id => 1, :content => 'This is a comment to article 1', :created_at => Time.now, :author_name => 'Mr. X')
+Comment.create(:article_id => 1, :content => 'This is another comment to article 1', :created_at => Time.now, :author_name => 'Mr. X')
+
+# Uncommenting below line should give ForeignKeyViolation if article_id = 0 doesn't exist
+# Comment.create(:article_id => 0, :content => 'This is another comment to article 1', :created_at => Time.now, :author_name => 'Mr. X')
+
+puts "Comments count in DB: #{Comment.count}"
